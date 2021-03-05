b5c6f15828d1185fb77186d4758f0c7f2ef9d66f
Johnny Squardov <johnny@johnmichael.dev>
2021-03-05 10:46:21 -0500

    create Login form and axios request to /auth/login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
-import React from "react"
-import "./App.css"
+import React from "react";
+import "./App.css";
 
-import Eventlite from "./components/Eventlite"
+import Login from "./components/Login";
+import Eventlite from "./components/Eventlite";
 
 const App = () => {
 	return (
 		<div className="App">
+			<Login />
 			<Eventlite />
 		</div>
-	)
-}
+	);
+};
 
-export default App
+export default App;
